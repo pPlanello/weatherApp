98019ad3a7759321159117da89c3374a904477b4
pPlanello <planello96@gmail.com>
2022-05-12 18:41:12 +0200

    Create Search menu

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const { inquirerMenu, stop } = require("./helpers/inquirer");
-
-
+const { inquirerMenu, stop, readInput } = require("./helpers/inquirer");
+const Searches = require("./models/searches");
 
 const main = async() => {
+    
+    const searches = new Searches();
     let option;
 
     do {
 
         option = await inquirerMenu();
-        
-        console.log({option});
+
+        switch(option) {
+            case 1:
+                // Show message
+                const place = await readInput('Ciudad: ');
+
+                // Show results
+                console.log('\n Información de la ciudad\n'.green);
+                console.log('Ciudad: ');
+                break;
+            case 2:
+                break;
+        }
         
         if (option !== 0) {
             await stop();
---
+++ models/searches.js
@@ -0,0 +1,14 @@
+class Searches {
+
+    history = [];
+
+    constructor() {}
+
+    async city(place = '') {
+        console.log('Place', place);
+
+        
+    }
+}
+
+module.exports = Searches;
